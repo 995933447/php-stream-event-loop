f7fc5397735a1bc8a8e7611908a01f5ae83849e3
995933447 <34621597+995933447@users.noreply.github.com>
2020-03-13 13:44:08 +0800

    Update timer.php

--- examples/timer.php
+++ examples/timer.php
@@ -4,7 +4,7 @@ require __DIR__ . "/../vendor/autoload.php";
 use Bobby\StreamEventLoop\LoopContract as Loop;
 use Bobby\StreamEventLoop\LoopFactory;
 
-$loop = LoopFactory::make(LoopFactory::SELECT_LOOP);
+$loop = LoopFactory::make();
 
 $loop->addTick(0.5, function (int $timerId, Loop $loop) {
     echo "Tick id: $timerId\n";
